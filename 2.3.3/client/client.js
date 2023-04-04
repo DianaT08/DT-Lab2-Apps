011464d4778f8344ece6623db88e8c6cb75e6fe8
DianaT08 <95448880+DianaT08@users.noreply.github.com>
2023-04-04 23:28:55 +0300

    Add files via upload

---
+++ 2.3.3/client/client.js
@@ -0,0 +1,42 @@
+var socket = io.connect('localhost:8000');
+
+try {
+    socket.on('connect', function (data) {
+        socket.emit("message-from-client", "Hello to everyone from " + checkBrowser());
+        console.log("client started");
+    });
+
+    socket.on('message-from-server', function (message) {
+        console.log("received message: "+message);
+        displayMessage("received message: " + message);
+    });
+
+}
+catch (err) {
+    alert('ERROR: socket.io encountered a problem:\n\n' + err);
+}
+
+function checkBrowser() {
+    var browser = 'Noname browser';
+    if (navigator.userAgent.search("Chrome") > -1) {
+        browser = "Chrome";
+    }
+    if (navigator.userAgent.search("Firefox") > -1) {
+        browser = "Firefox";
+    }
+    return browser;
+}
+
+document.getElementById("send").addEventListener("click", sendMessage);
+function sendMessage() {
+    var message = document.getElementById("message").value;
+    socket.emit("message-from-client", message);
+    console.log("sent message: "+message);
+    displayMessage("sent message: " + message);
+}
+
+function displayMessage(message) {
+    var mes = document.createElement("p");
+    mes.innerHTML = message;
+    document.getElementById("chat").appendChild(mes);
+}
